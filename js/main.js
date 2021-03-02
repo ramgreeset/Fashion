e08cfec3f495eac9a2c024e8f00159103a305675
Artemyev <artemyev.ram@gmail.com>
2021-03-02 19:54:35 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
